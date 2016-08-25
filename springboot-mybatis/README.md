bc45b58924a5612ff15c44433b2f6abf88484dd0
xiaowing <wing0630@gmail.com>
2016-08-25 23:41:56 +0800

    add the implementation of <init-param> in web.xml as Java Config.

--- springboot-mybatis/README.md
+++ springboot-mybatis/README.md
@@ -2,13 +2,15 @@ springboot-mybatis
 =============
 
 ###简介
-基于Spring-boot技术，搭建了一个RESTful API的Demo。该Demo实现了以下特性：
+
+基于Spring-boot技术，搭建了一个RESTful API的Demo。该Demo实现了以下特性:
+
 1. 在Spring-boot中以纯Java Config的形式集成了mybatis(_ORM_) + druid(_DataSource_) + pgJdbc(_JDBC Driver_)
 2. 在该Spring-boot应用中启用了**Druid**自带的数据源监控Portal。
 
 ###学习点
 1. 将原本以xml形式进行配置的mybatis配置项在Spring Boot中用Java Config的形式进行配置
 2. 将原本以xml形式进行定义和注册的mybatis的映射关系(mapper)通过注解的形式定义在Spring-boot应用中
-3. 将原本需要在tomcat的*web.xml*文件中进行启用的**Druid**监控Portal通过`@Configuration`以及`@Bean`的注解形式注册至Spring-boot内置的Tomcat中
+3. 将原本需要在tomcat的*web.xml*文件中进行启用的**Druid**监控Portal通过`@Configuration`以及`@Bean`的注解形式注册至Spring-boot内置的Tomcat中，同时利用代码实现原本需要在*web.xml*中`<servlet>`下配置的`init-param`。
 4. 运用**Mockito**对mybatis的Mapper实现进行模拟，并以此做成单元测试。
 
--- springboot-mybatis/src/main/java/org/wingsdak/spbtmybatis/conf/ServletConfig.java
+++ springboot-mybatis/src/main/java/org/wingsdak/spbtmybatis/conf/ServletConfig.java
@@ -1,5 +1,9 @@
 package org.wingsdak.spbtmybatis.conf;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -11,13 +15,21 @@ import com.alibaba.druid.support.http.StatViewServlet;
 @Configuration
 @Import({EmbeddedServletContainerAutoConfiguration.class })
 public class ServletConfig {
+	
+	// 向内置容器定义Servlet
 	@Bean
 	public StatViewServlet dispatchDruidServlet() {
 		return new StatViewServlet();
 	}
 	
+	// 向内置容器注册上述Servlet以及该Servlet对应的url Mapping以及该Servlet初始化时的参数
 	@Bean
 	public ServletRegistrationBean registerServlet(){
-		return new ServletRegistrationBean(dispatchDruidServlet(), "/druid/*");
+		ServletRegistrationBean statViewServletRegistrationBean = 
+				new ServletRegistrationBean(dispatchDruidServlet(), "/druid/*");
+		statViewServletRegistrationBean.addInitParameter(StatViewServlet.PARAM_NAME_ALLOW,  "localhost/127.0.0.1");
+		statViewServletRegistrationBean.addInitParameter(StatViewServlet.PARAM_NAME_USERNAME, "admin");
+		statViewServletRegistrationBean.addInitParameter(StatViewServlet.PARAM_NAME_PASSWORD, "password");
+		return statViewServletRegistrationBean;
 	}
 }
